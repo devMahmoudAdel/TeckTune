213a0a437ae783648b82cf1c9ca6b2a4e7187f72
YousseefAh <yousseef.ah@gmail.com>
2025-02-28 17:25:28 +0200

    Resolve

--- Components/ProductDetails/ProductDetails.jsx
+++ Components/ProductDetails/ProductDetails.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Text, View ,StyleSheet,Image,Pressable,StatusBar} from "react-native";
-import TopsectionPD from "./topsectionPD";
-import BottomsectionPD from "./bottomsectionPD";
+import TopsectionPD from "./TopsectionPD";
+import BottomsectionPD from "./BottomsectionPD";
 export default function ProductDetails(props) {
   const { navigation} = props;
   const { title ,price,images,rating,colors,description} = props.route.params;
   return (
     <View style={[styles.container, { marginTop: StatusBar.currentHeight }]}>
 
-      <TopsectionPD title={title} images={images} rating={rating} price={price} colors={colors} description={description} style={styles.topsection}/>
+      {/* <TopsectionPD title={title} images={images} rating={rating} price={price} colors={colors} description={description} style={styles.topsection} /> */}
+
+      <TopsectionPD images={images}  style={styles.topsection} />
+
       <BottomsectionPD title={title} images={images} rating={rating} price={price} colors={colors} description={description} style={styles.bottomsection}/>
     </View>
   );
