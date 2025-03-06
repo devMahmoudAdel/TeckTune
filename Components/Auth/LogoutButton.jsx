e8c41a6e2a108aa1f6e4f12bb228775d75ecae02
Abdelrahman Medhat <a.medhatahmed9@gmail.com>
2025-03-06 11:13:52 +0200

    log out has been added

---
+++ Components/Auth/LogoutButton.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { TouchableOpacity, Pressable, Text, StyleSheet } from "react-native";
+import { AuthContext } from "../../context/AuthContext";
+
+import ProfileTags from "../ProfileTags";
+
+const LogoutButton = () => {
+  const { logout } = useContext(AuthContext);
+
+  return (
+    <TouchableOpacity onPress={logout}>
+      <ProfileTags name="Log Out" image={"Log-out-sharp"} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#FF3B30",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default LogoutButton;
--- Components/Profile.jsx
+++ Components/Profile.jsx
@@ -1,54 +1,44 @@
-import { Text, View,Image,StyleSheet,TouchableOpacity } from "react-native";
-import ProfileTags from "./ProfileTags"; 
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import ProfileTags from "./ProfileTags";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {
-  NavigationContainer,
-  NavigationIndependentTree,
-} from "@react-navigation/native";
-import About from "./About";
-import Cart from "./Cart";
-import Checkout from "./Checkout";
-import Home from "./Home";
-import ProductList from "./ProductList";
-import ProductDetails from "./ProductDetails/ProductDetails";
-import Settings from "./Settings";
 
+import LogoutButton from "./Auth/LogoutButton";
 
 export default function Profile({ navigation }) {
-  return (      
-      <View style={styles.container}>
-        {/* <Indexes navigation={navigation} /> */}
-        <View style={{ alignItems: "center", marginVertical: 20 }}>
-          {/* <Image
+  return (
+    <View style={styles.container}>
+      {/* <Indexes navigation={navigation} /> */}
+      <View style={{ alignItems: "center", marginVertical: 20 }}>
+        {/* <Image
           source={require("../assets/icon.png")}
           style={styles.imageProfile}
         /> */}
-          <Ionicons
-            name="person"
-            size={100}
-            color="black"
-            style={styles.imageProfile}
-          />
-          <Text style={styles.userText}>User Name</Text>
-          <Text style={styles.emailText}>user-email@email.com</Text>
-        </View>
-        <TouchableOpacity onPress={() => navigation.navigate("MyProfile")}>
-          <ProfileTags name="Profile" image={"person"} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
-          <ProfileTags name="Settings" image={"settings-sharp"} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("About")}>
-          <ProfileTags name="About" image={"information-circle-sharp"} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ProfileTags name="Share App" image={"share-social-sharp"} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Help")}>
-          <ProfileTags name="Help" image={"help-circle-sharp"} />
-        </TouchableOpacity>
+        <Ionicons
+          name="person"
+          size={100}
+          color="black"
+          style={styles.imageProfile}
+        />
+        <Text style={styles.userText}>User Name</Text>
+        <Text style={styles.emailText}>user-email@email.com</Text>
       </View>
+      <TouchableOpacity onPress={() => navigation.navigate("MyProfile")}>
+        <ProfileTags name="Profile" image={"person"} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
+        <ProfileTags name="Settings" image={"settings-sharp"} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("About")}>
+        <ProfileTags name="About" image={"information-circle-sharp"} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ProfileTags name="Share App" image={"share-social-sharp"} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Help")}>
+        <ProfileTags name="Help" image={"help-circle-sharp"} />
+      </TouchableOpacity>
+      <LogoutButton/>
+    </View>
   );
 }
 const styles = StyleSheet.create({
