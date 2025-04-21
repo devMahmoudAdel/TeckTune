5cb8e002e8fec1bce1d73cc736b2eaa7bbcf1bf4
B A D R <badrrbadrr92@gmail.com>
2025-04-21 18:12:05 +0200

    adding upload and delete img by supabase

---
+++ supabase/initSupabase.js
@@ -0,0 +1,28 @@
+import * as SecureStore from 'expo-secure-store';
+import 'react-native-url-polyfill/auto';
+
+import { createClient } from '@supabase/supabase-js';
+
+// Use a custom secure storage solution for the Supabase client to store the JWT
+const ExpoSecureStoreAdapter = {
+  getItem: (key) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key, value) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const url = "https://ektldysacqkendukfvkt.supabase.co";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrdGxkeXNhY3FrZW5kdWtmdmt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMjY0MzQsImV4cCI6MjA2MDgwMjQzNH0.RdQOVzEWm8GeijaRaKkhxu-PTI0yJGppeO7bmdDkv0s";
+
+// Initialize the Supabase client
+export const supabase = createClient(url, key, {
+  auth: {
+    storage: ExpoSecureStoreAdapter,
+    detectSessionInUrl: false,
+  },
+});
