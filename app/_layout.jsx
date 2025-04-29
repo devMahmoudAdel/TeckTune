fcb06d1f27393883bcd0d9c3d9ea9c11976c04e3
Mahmoud Adel <karim3del04@gmail.com>
2025-04-29 15:50:28 +0300

    hidden splash's header

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,6 +5,7 @@ export default function RootLayout() {
   return (
     <AuthProvider>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(main)" options={{ headerShown: false }} />
         <Stack.Screen
